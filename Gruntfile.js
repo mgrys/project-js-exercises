9ad2955d8cab130348b8ded53ee8c0898128f7c8
Marek Grys <marek.grys@gmail.com>
2017-07-25 23:37:25 +0200

    Create index, css and js file. Add some basic javascript code, configurate local grunt plugins

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.sass'
+        }
+      }
+    },
+
+watch: {
+    scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    }
+},
+
+browserSync: {
+  default_options: {
+    bsFiles: {
+      src: [
+        "css/*.css",
+        "*.html"
+      ]
+    },
+    options: {
+      watchTask: true,
+      server: {
+        baseDir: "./"
+      }
+    }
+  }
+},
+
+imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
+    }
+}
+  });
+
+
+  // Load the plugins tasks
+grunt.loadNpmTasks('grunt-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-browser-Sync');
+
+  // Default task(s).
+  grunt.registerTask('default', ['browserSync', 'sass', 'watch', 'imagemin']);
+};
