76c5b6929cbe29fbf8ed2498c11ac8b2321ec33d
Marek Grys <marek.grys@gmail.com>
2017-07-26 12:37:46 +0200

    add comments task 8.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+//zmienna - pobranie imienia
 var name = prompt('Enter your name');
+//funkcja alert pokazująca na ekranie komunikat + wartość zmiennej
 alrt('Hello, ' + name);
+/* wyrzucenie zmiennej
+do konsoli */
 consol.log('Hello wold, My name is ' + name);
