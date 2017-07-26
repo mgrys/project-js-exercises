3cb7746d0cf652590c4b41e1436e30112cd34539
Marek Grys <marek.grys@gmail.com>
2017-07-26 14:31:18 +0200

    add task 8.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,76 @@
 module.exports = function(grunt) {
+
   // Project configuration.
   grunt.initConfig({
-    sass: {
+sass: {
       options: {
         sourceMap: true
       },
       dist: {
         files: {
-          "css/main.css": "sass/main.sass"
+          'css/main.css': 'sass/main.sass'
         }
       }
     },
 
-    jshint: {
-      all: ["js/*.js"]
-    },
+ jshint: {
+      all: ['js/*.js']
+  },
 
-    watch: {
-      scripts: {
-        files: ["js/*.js"],
-        tasks: ["jshint"],
-        options: {
-          spawn: false
-        }
-      },
-      styles: {
-        files: ["sass/*.sass"],
-        tasks: ["sass"],
-        options: {
-          spawn: false
-        }
-      }
+watch: {
+  scripts: {
+    files: ['js/*.js'],
+    tasks: ['jshint'],
+    options: {
+      spawn: false,
     },
-
-    browserSync: {
-      default_options: {
-        bsFiles: {
-          src: ["css/*.css", "*.html"]
-        },
-        options: {
-          watchTask: true,
-          server: {
-            baseDir: "./"
-          }
-        }
-      }
+  },
+  styles: {
+    files: ['sass/*.sass'],
+    tasks: ['sass'],
+    options: {
+      spawn: false,
     },
+  }
+},
 
-    imagemin: {
-      dynamic: {
-        files: [
-          {
-            expand: true,
-            cwd: "images/",
-            src: ["**/*.{png,jpg,gif}"],
-            dest: "images/build/"
-          }
-        ]
+browserSync: {
+  default_options: {
+    bsFiles: {
+      src: [
+        "css/*.css",
+        "*.html"
+      ]
+    },
+    options: {
+      watchTask: true,
+      server: {
+        baseDir: "./"
       }
     }
+  }
+},
+
+imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
+    }
+}
   });
 
+
   // Load the plugins tasks
-  grunt.loadNpmTasks("grunt-sass");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-imagemin");
-  grunt.loadNpmTasks("grunt-browser-Sync");
+grunt.loadNpmTasks('grunt-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-browser-Sync');
 
   // Default task(s).
-  grunt.registerTask("default", [
-    "browserSync",
-    "sass",
-    "watch",
-    "jshint",
-    "imagemin"
-  ]);
+  grunt.registerTask('default', ['browserSync', 'sass','jshint', 'imagemin', 'watch']);
 };
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 //zmienna - pobranie imienia
-var name = prompt('Enter your name');
+//var name = prompt('Enter your name');
 //funkcja alert pokazująca na ekranie komunikat + wartość zmiennej
-alrt('Hello, ' + name);
+//alert('Hello, ' + name);
 /* wyrzucenie zmiennej
 do konsoli */
-consol.log('Hello wold, My name is ' + name);
+//consol.log('Hello wold, My name is ' + name);
+
+//zadanie 8.5
+
+var a = prompt('Podaj bok trójkąta');
+var h= prompt('Podaj wysokośc trójkąta');
+triangleArea= a*h/2;
+alert ('Pole trójkąta wynosi' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
