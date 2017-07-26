f0cddb1ae9941f93e608e609db4d5a903dd07cde
Marek Grys <marek.grys@gmail.com>
2017-07-26 11:50:41 +0200

    change in gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,79 @@
 module.exports = function(grunt) {
-
   // Project configuration.
   grunt.initConfig({
-sass: {
+    sass: {
       options: {
         sourceMap: true
       },
       dist: {
         files: {
-          'css/main.css': 'sass/main.sass'
+          "css/main.css": "sass/main.sass"
         }
       }
     },
 
-watch: {
-    scripts: {
-        files: ['sass/*.sass'],
-        tasks: ['sass'],
-        options: {
-            spawn: false,
-        },
-    }
-},
-
     jshint: {
-      all: ['js/*.js']
-  },
-
-browserSync: {
-  default_options: {
-    bsFiles: {
-      src: [
-        "css/*.css",
-        "*.html"
-      ]
+      all: ["js/*.js"]
     },
-    options: {
-      watchTask: true,
-      server: {
-        baseDir: "./"
+
+    watch: {
+      scripts: {
+        files: ["js/*.js"],
+        tasks: ["jshint"],
+        options: {
+          spawn: false
+        }
+      },
+      styles: {
+        files: ["sass/*.sass"],
+        tasks: ["sass"],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: ["css/*.css", "*.html"]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [
+          {
+            expand: true,
+            cwd: "images/",
+            src: ["**/*.{png,jpg,gif}"],
+            dest: "images/build/"
+          }
+        ]
       }
     }
-  }
-},
-
-imagemin: {
-    dynamic: {
-        files: [{
-            expand: true,
-            cwd: 'images/',
-            src: ['**/*.{png,jpg,gif}'],
-            dest: 'images/build/'
-        }]
-    }
-}
   });
 
-
   // Load the plugins tasks
-grunt.loadNpmTasks('grunt-sass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.loadNpmTasks('grunt-contrib-imagemin');
-grunt.loadNpmTasks('grunt-browser-Sync');
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-browser-Sync");
 
   // Default task(s).
-  grunt.registerTask('default', ['browserSync', 'sass', 'watch','jshint', 'imagemin']);
+  grunt.registerTask("default", [
+    "browserSync",
+    "sass",
+    "watch",
+    "jshint",
+    "imagemin"
+  ]);
 };
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello wold, My name is ' + name);
+alrt('Hello, ' + name);
+consol.log('Hello wold, My name is ' + name);
